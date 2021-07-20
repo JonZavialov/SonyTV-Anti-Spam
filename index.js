a78dbb4ccd5d835ebe87342776d0db2d040e8e20
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-07-19 23:46:19 -0400

    added token json

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs')
+
+let rawdata = fs.readFileSync('./logins.json');
+let logins = JSON.parse(rawdata);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.channel.send('pong');
+  }
+});
+
+client.login(logins.token);
