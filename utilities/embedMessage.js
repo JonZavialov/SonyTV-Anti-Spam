89b9f674f7a78756a926f9d996d5ca7bffa01a98
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-07-22 03:34:30 -0400

    changed embed color

--- utilities/embedMessage.js
+++ utilities/embedMessage.js
@@ -11,7 +11,7 @@ function embedMessage(title,content){
     const embed = new Discord.MessageEmbed()
     .setTitle(title)  
     .setImage(content)
-    .setColor(0x252526)
+    .setColor(0x3B4B7F)
     .setFooter('Bryson Bot');
 
     return embed
