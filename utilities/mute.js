97e57b9e883e4cf95ad711a0bce2d70b947d0b8a
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-08-09 06:39:28 -0400

    created handler for already muted

--- utilities/mute.js
+++ utilities/mute.js
@@ -21,17 +21,22 @@ function mute(msg, client){
         handleErrorTooManyArgs(msg)
         return
     }
-    
+
+    let muteID = logins.muteRole
+
     let mention = msg.content.substring(10,msg.content.length)
     let id = mention.substring(3,mention.length-1)
     
     const guild = client.guilds.cache.get(logins.server) // get the guild object
-    let muteRole = guild.roles.cache.find(r => r.id === logins.muteRole)
+    let muteRole = guild.roles.cache.find(r => r.id === muteID)
 
     let thanos = client.users.fetch(id);
     thanos.then(function(user) {
         const member = guild.member(user) // convert the User object to a GuildMember
-        if(!member){
+        if(member.roles.cache.has(muteID)){
+            msg.lineReply(`${mention} is already muted!`)
+        }
+        else if(!member){
             msg.lineReply('User not found!')
         }else member.roles.add(muteRole)
     })
