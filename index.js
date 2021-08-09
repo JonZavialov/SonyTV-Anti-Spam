d499f42d76e6b5d29783c34b03d8c21471db0689
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-08-09 02:45:20 -0400

    added mp3 and playing when someone speaks

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const Discord = require('discord.js')
 require('discord-reply')
 const client = new Discord.Client()
 const fs = require('fs')
+var opus = require('node-opus');
 
 const prefix = "sony "
 
--- utilities/vcListen.js
+++ utilities/vcListen.js
@@ -1,10 +1,12 @@
 async function vcListen(msg){
     const connection = await msg.member.voice.channel.join()
-    
+
     connection.on('speaking', (user, speaking) => {
         var isSpeaking
+        
         if(speaking.bitfield == 1){
             isSpeaking = true
+            const dispatcher = connection.play('C:\\Users\\jzavy\\Desktop\\SonyTV-Anti-Spam\\audios\\shut.mp3')
         }
         else{
             isSpeaking = false
